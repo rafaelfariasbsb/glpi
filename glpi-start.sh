667b92d2f2553ab7fc4281ba977891315020bdef
rafaelfariasbsb <rfariasbsb@gmail.com>
2023-08-13 20:15:31 -0300

    new version

--- glpi-start.sh
+++ glpi-start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+[[ ! "$VERSION_GLPI" ]] \
+	&& VERSION_GLPI=$(curl -s https://api.github.com/repos/glpi-project/glpi/releases/latest | grep tag_name | cut -d '"' -f 4)
+
+if [[ -z "${TIMEZONE}" ]]; then echo "TIMEZONE is unset"; 
+else 
+    sed -i "/;date.timezone =/c\date.timezone = $TIMEZONE" /etc/php.ini
+fi
+
+sed -i "/upload_max_filesize =/c\upload_max_filesize = $UPLOAD_MAX_FILESIZE" /etc/php.ini 
+sed -i "/post_max_size = /c\post_max_size = $POST_MAX_SIZE" /etc/php.ini
 sed -i "/ServerName/c\ServerName $HOSTNAME" /etc/httpd/conf.d/ssl.conf
 sed -i "/SSLCertificateFile/c\SSLCertificateFile /etc/pki/tls/certs/$SSLCERTIFICATEFILE" /etc/httpd/conf.d/ssl.conf
 sed -i "/SSLCertificateKeyFile/c\SSLCertificateKeyFile /etc/pki/tls/private/$SSLCERTIFICATEKEYFILE" /etc/httpd/conf.d/ssl.conf
